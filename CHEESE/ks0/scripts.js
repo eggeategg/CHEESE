d7dc9d90bea8c6a443c359112a09cd1505e29152
eggeategg <45055954+eggeategg@users.noreply.github.com>
2025-04-12 21:48:12 -0700

    object creation and methods
    creating an object called an image card to store some data that will be used to create cards later on in html, and some basic methods to retrieve data as well as an array to store the cards for later when they pop up on screen

--- CHEESE/ks0/scripts.js
+++ CHEESE/ks0/scripts.js
@@ -23,12 +23,48 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+class imageCard {
+  constructor(title, image, description) {
+    this.title = title;
+    this.image = image;
+    this.description = description;
+    
+  }
+  //here are a couple of methods to gather data I made in the begining
+  getName() {
+    return this.title;
+  }
+  getImage() {
+    return this.image;
+  }
+  getDes() {
+    return this.description;
+  }
+}
+
+let imageCards = [
+  new imageCard(
+    falling,
+    "f",
+    "description"
+  ),
+  new imageCard(
+    falling,
+    "f",
+    "description"
+  ),
+  new imageCard(
+    falling,
+    "f",
+    "description"
+  ),
+  new imageCard(
+    falling,
+    "f",
+    "description"
+  )
+]
+
 
 // This is an array of strings (TV show titles)
 let titles = [
